ee0482a9efb6a5ba168084ae7319a12763e5fed6
tung1100i <1851063190@e.tlu.edu.vn>
2022-06-04 23:23:28 +0700

    push code

---
+++ Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace BaiTapLon
+{
+    partial class GamePlay_UI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GamePlay_UI));
+            this.TimeBreak = new System.Windows.Forms.Timer(this.components);
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Exit = new System.Windows.Forms.Button();
+            this.Start = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chọnĐộKhóToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dễToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vừaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.khóToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TABLE = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TimeBreak
+            // 
+            this.TimeBreak.Enabled = true;
+            this.TimeBreak.Interval = 150;
+            this.TimeBreak.Tick += new System.EventHandler(this.TimeBreak_Tick);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.textBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.Exit);
+            this.splitContainer1.Panel1.Controls.Add(this.Start);
+            this.splitContainer1.Panel1.Controls.Add(this.menuStrip1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.TABLE);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 265;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(59, 169);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(151, 22);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.Text = "0";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 134);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Điểm Số";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Thời Gian";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(239, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(25, 25);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "?";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 89);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(151, 22);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(89, 367);
+            this.Exit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(88, 39);
+            this.Exit.TabIndex = 2;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(89, 295);
+            this.Start.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(88, 46);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(265, 28);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.chọnĐộKhóToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // chọnĐộKhóToolStripMenuItem
+            // 
+            this.chọnĐộKhóToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dễToolStripMenuItem,
+            this.vừaToolStripMenuItem,
+            this.khóToolStripMenuItem});
+            this.chọnĐộKhóToolStripMenuItem.Name = "chọnĐộKhóToolStripMenuItem";
+            this.chọnĐộKhóToolStripMenuItem.Size = new System.Drawing.Size(178, 26);
+            this.chọnĐộKhóToolStripMenuItem.Text = "Chọn Độ khó";
+            // 
+            // dễToolStripMenuItem
+            // 
+            this.dễToolStripMenuItem.Name = "dễToolStripMenuItem";
+            this.dễToolStripMenuItem.Size = new System.Drawing.Size(118, 26);
+            this.dễToolStripMenuItem.Text = "Dễ";
+            this.dễToolStripMenuItem.Click += new System.EventHandler(this.dễToolStripMenuItem_Click);
+            // 
+            // vừaToolStripMenuItem
+            // 
+            this.vừaToolStripMenuItem.Name = "vừaToolStripMenuItem";
+            this.vừaToolStripMenuItem.Size = new System.Drawing.Size(118, 26);
+            this.vừaToolStripMenuItem.Text = "Vừa";
+            this.vừaToolStripMenuItem.Click += new System.EventHandler(this.vừaToolStripMenuItem_Click);
+            // 
+            // khóToolStripMenuItem
+            // 
+            this.khóToolStripMenuItem.Name = "khóToolStripMenuItem";
+            this.khóToolStripMenuItem.Size = new System.Drawing.Size(118, 26);
+            this.khóToolStripMenuItem.Text = "Khó";
+            this.khóToolStripMenuItem.Click += new System.EventHandler(this.khóToolStripMenuItem_Click);
+            // 
+            // TABLE
+            // 
+            this.TABLE.BackgroundImage = global::BaiTapLon.Properties.Resources.back;
+            this.TABLE.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.TABLE.ColumnCount = 6;
+            this.TABLE.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.TABLE.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.TABLE.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.TABLE.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.TABLE.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.TABLE.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.TABLE.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TABLE.Location = new System.Drawing.Point(0, 0);
+            this.TABLE.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.TABLE.Name = "TABLE";
+            this.TABLE.RowCount = 6;
+            this.TABLE.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.TABLE.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.TABLE.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.TABLE.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.TABLE.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.TABLE.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.TABLE.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.TABLE.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.TABLE.Size = new System.Drawing.Size(531, 450);
+            this.TABLE.TabIndex = 0;
+            this.TABLE.Paint += new System.Windows.Forms.PaintEventHandler(this.TABLE_Paint);
+            // 
+            // GamePlay_UI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.Name = "GamePlay_UI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Memory Game";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem chọnĐộKhóToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dễToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vừaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem khóToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer TimeBreak;
+        private System.Windows.Forms.TableLayoutPanel TABLE;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTapLon
+{
+    public partial class GamePlay_UI : Form
+    {
+        public GamePlay_UI()
+        {
+            InitializeComponent();
+        }
+        private System.Windows.Forms.Timer aTimer;
+        SoundPlayer sp = new SoundPlayer();
+        private int counter;// thời gian đếm
+        List<List<PictureBox>> mt;
+        int time = 300;//thời gian đếm ngược(mặc định=300s:độ khó dễ)
+        int y = 10;
+        private void Start_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "0";           
+            //bộ đếm thời gian
+            counter = time;//thời gian đếm ngược
+            mt = new List<List<PictureBox>>();
+            TABLE.Controls.Clear();//xóa trắng bảng chọn(tránh chồng dối tượng)
+            aTimer = new System.Windows.Forms.Timer();//khởi tạo đối tượng đếm
+            aTimer.Tick += new EventHandler(aTimer_Tick);
+            aTimer.Interval = 1000; // thời gian đếm bằng 1s(1000ms)
+            aTimer.Start();//bắt đầu bộ đếm
+            textBox1.Text = counter.ToString();//chuyển tg đếm lên form qua textbox
+            //-------------------------------------------------------------------------------------------------------
+            //Sắp xếp ảnh ngẫu nhiên trong bảng chọn ảnh
+            Random random = new Random();//biến tạo ngẫu nhiên
+            int[] anh = new int[36];//mảng lưu hình
+            int[] dem = new int[18];//mảng lưu bộ đếm số lần xh của từng hình
+            int k = 0;
+            for (int i = 0; i < 18; i++) dem[i] = 0;
+            while (k < 36)
+            {
+                int x = random.Next(0, 17);//random 1 số từ 0-17
+                if (dem[x] < 2)//nếu số lần xh của số vừa random nhỏ hơn 2 thì thực hiện công việc
+                {
+                    dem[x]++;//tăng số lần xh thêm 1
+                    anh[k] = x + 1;//thêm số random vào mảng ảnh
+                    k++;//tăng chỉ số của mảng
+                }
+                else
+                    x = random.Next(0, 17);//random lại 1 số khác(số vừa random đã xh 2 lần)
+                if (k > 20)//khi k lớn hơn 20 thực hiện việc sau
+                {
+                    for (int j = 0; j < 18; j++)//chạy số từ 0-17
+                        if (dem[j] < 2)//nếu số lần xh nhỏ hơn 2 thì thêm vào mảng ảnh
+                        {
+                            dem[j]++;
+                            anh[k] = j + 1;
+                            k++;
+                        }
+                }
+            }
+            k = 0;          
+            //thiết lập nhóm hình ảnh vào bảng
+            PictureBox t1 = new PictureBox() { Width = 91, Height = 77, Location = new Point(3, 3) };//picture box gốc
+            for (int i = 0; i < 6; i++)
+            {
+                mt.Add(new List<PictureBox>());//thêm picture box mới vào list
+                for (int j = 0; j < 6; j++)
+                {
+                    //thiết lập các button và thuộc tính của từng picture box
+                    PictureBox pb = new PictureBox()
+                    {
+                        Width = 91,//thiết lập width
+                        Height = 77,//thiết lập height
+                        
+                        Location = new Point(t1.Location.X + i * Width, t1.Location.Y + j * Height),//thiết lập vị trí của button
+                        BackgroundImageLayout = ImageLayout.Stretch,//định dạng ảnh nền
+                        BackgroundImage = Image.FromFile(Application.StartupPath + "\\Resources\\" + Convert.ToString(anh[k]) + ".jpg"),//thiết lập ảnh nền(ảnh cần so sánh)
+                        Image = Image.FromFile(Application.StartupPath + "\\Resources\\back.jpg"),//thiết lập ảnh che hình
+                        Tag = anh[k]
+                    };
+                    k++;
+                    pb.Click += Pb_Click;
+                    TABLE.Controls.Add(pb);//thêm picture box đã thiết lập vào bảng chọn
+                }
+            }
+        }
+        Boolean kt = true;
+        int m=20,n=21;
+        PictureBox t2 = new PictureBox();
+        PictureBox t3 = new PictureBox();
+        //quy tắc trò chơi
+        private void Pb_Click(object sender, EventArgs e)
+        {
+            PictureBox pb = sender as PictureBox;
+            pb.Image = null;
+            if (kt == true)
+            {
+                t2 = pb;
+                kt = false;
+            }
+            else if(pb.Location != t2.Location)
+            {
+                t3 = pb;
+                kt = false;
+            }
+            else
+            {
+                kt = false;
+            }
+            if ((kt==false) )
+            {
+                if (t2 != null && t3 != null)
+                {
+                    m = int.Parse(t2.Tag.ToString());
+                    n = int.Parse(t3.Tag.ToString());
+                    TimeBreak.Start();
+                }            
+            }           
+        }
+        private void aTimer_Tick(object sender, EventArgs e)
+        {
+            counter--;
+            if (counter < 0)
+                aTimer.Stop();
+            else
+                textBox1.Text = counter.ToString();
+        }
+        //nút exit
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        //nút hướng dẫn
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string rule = "QUY TẮC TRÒ CHƠI:\n"
+                +"Người chơi chọn hình trên bảng hình, nếu hình giống nhau sẽ biến mất,còn không sẽ bị che lại\n"
+                +" Nhiệm vụ của ng chơi là làm biến mất tất cả hình trong thời gian giới hạn\n Chúc các bạn chơi game vui vẻ!!";
+            MessageBox.Show(rule);
+        }
+        //__________________________________________________________________________________
+        //nhóm phím chọn độ khó, sau khi chọn tự động bắt đầu game, độ khó mặc định là dễ
+        private void dễToolStripMenuItem_Click(object sender, EventArgs e){time = 5;Start_Click(null,null);y = 10;}        
+        private void vừaToolStripMenuItem_Click(object sender, EventArgs e){time = 200; Start_Click(null, null);y = 20;}
+        private void khóToolStripMenuItem_Click(object sender, EventArgs e){time = 100; Start_Click(null, null);y = 40;}
+        //show messbox báo đã thua do hết giờ, đồng thời xóa màn hình trò chơi
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (int.Parse(textBox1.Text) == 0)
+            {
+                aTimer.Stop();//ngừng bộ đếm khi tg bằng 0
+                MessageBox.Show("Bạn Thua!!");
+                TABLE.Controls.Clear();
+            }
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (int.Parse(textBox2.Text) == 360)
+            {
+                aTimer.Stop();
+                textBox2.Text = Convert.ToString(int.Parse(textBox2.Text) + y * int.Parse(textBox1.Text));
+                MessageBox.Show("Bạn Thắng!!\nĐiểm số của bạn là: " + textBox2.Text);
+                TABLE.Controls.Clear();
+            }
+        }
+
+        private void TABLE_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void TimeBreak_Tick(object sender, EventArgs e)
+        {
+            if(m==n)
+            {
+                t2.BackgroundImage = null;
+                t3.BackgroundImage = null;
+                textBox2.Text = Convert.ToString(int.Parse(textBox2.Text) + 20);
+                t2 = null;
+                t3 = null;
+                kt = true;
+            }
+            else
+            {
+                kt = true;
+                t3.Image = Image.FromFile(Application.StartupPath + "\\Resources\\back.jpg");
+                t2.Image = Image.FromFile(Application.StartupPath + "\\Resources\\back.jpg");
+                t2 = null; t3 = null;
+            }
+            TimeBreak.Stop();           
+        }
+    }
+    
+}
